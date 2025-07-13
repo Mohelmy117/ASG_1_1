e472e531a0f7e7e386c96f89266c2fd5f238f90c
Mohelmy117 <elmymoh3@gmail.com>
2025-07-13 09:35:29 +0300

    Add files via upload

---
+++ asg_1_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int  value ,x,a;
+x = value/2;
+a= x*2;
+printf("Enter number: ");
+fflush (stdin);fflush (stdout);
+scanf("%d",&value);
+if (a==value)
+{printf ("The odd is 0\nThe even is 1");}
+else 
+{printf ("The odd is 1\nThe even is 0");}
+
+
+}
